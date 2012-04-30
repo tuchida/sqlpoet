e75a8c68d4877139cfd7667aef54d5e85de2dac8
Your Name <dc578av_adle@yahoo.co.jp>
2012-04-30 15:20:59 +0900

    x

--- sqlpoet.js
+++ sqlpoet.js
@@ -61,9 +61,8 @@ SQLPoet.create = (function() {
     return this.sql_;
   };
 
-  function call_(value, options) {
+  function call_(value, var_args) {
     var fnName = this.lastToken_;
-    var name = (options && options.name) || fnName;
     if (/\$$/.test(fnName)) {
       this.lastToken_ = String.replace(fnName, /\$$/, '');
       append_.call(this);
@@ -74,31 +73,9 @@ SQLPoet.create = (function() {
       }
 // todo support Date
     } else if (/^\$/.test(fnName)) {
-      name = String.replace(name, /^\$/, '');
-      if (fnName == '$table') {
-        this.lastToken_ = '${TABLE_NAME(' + value + ')}';
-      } else if (fnName == '$schemaId') {
-        this.lastToken_ = '${SCHEMA_ID(' + value + ')}';
-      } else {
-        if (!Array.isArray(value)) {
-          value = [value];
-        }
-        var type = 'string';
-        if (options && options.type) {
-          type = options.type;
-        } else if (value.length > 0) {
-          if (SQLPoet.isNumber(value[0])) {
-            type = 'int';
-          }
-// todo support Date
-        }
-        if (type == 'int') {
-          this.args_.put(name, SQLPoet.naInt(value));
-          this.lastToken_ = '${VAR(' + name + ', int)}';
-        } else {
-          this.args_.put(name, SQLPoet.naString(value));
-          this.lastToken_ = '${VAR(' + name + ', string)}';
-        }
+      var token = handleFunction_(fnName, this.args_, Array.prototype.slice.call(arguments));
+      if (token) {
+        this.lastToken_ = token;
       }
     } else {
       append_.call(this);
@@ -109,6 +86,140 @@ SQLPoet.create = (function() {
     }
     append_.call(this);
   };
+
+  function handleFunction_(fnName, viewArgs, args) {
+    if (fnName == '$stringList') {
+      return handleListFunction_('string', viewArgs, args);
+    } else if (fnName == '$intList') {
+      return handleListFunction_('int', viewArgs, args);
+    } else if (fnName == '$list') {
+      return handleRawListFunction_(viewArgs, args);
+    } else if (fnName == '$baseTable') {
+      return '${BASE_TABLE()}';
+    } else if (fnName == '$table') {
+      return '${TABLE_NAME(' + args[0] + ')}';
+    } else if (fnName == '$schemaId') {
+      return '${SCHEMA_ID(' + args[0] + ')}';
+    } else if (fnName == '$define') {
+// todo
+    } else {
+      var value = args[0];
+      var options = args[1];
+      var name = (options && options.name) || fnName;
+      name = String.replace(name, /^\$/, '');
+      var type;
+      if (options && options.type) {
+        type = options.type;
+      } else if (value != null) {
+        type = typeOf_(value);
+// todo support Date
+      }
+      // if (fnName == '$var') {
+      //   name = options && options.name;
+      // }
+      // if (!name) {
+      //   name = generator();
+      // }
+      if (value != null) {
+        addViewArgs_(viewArgs, type, name, value);
+      }
+      if (type == 'int') {
+        return '${VAR(' + name + ', int)}';
+      } else {
+        return '${VAR(' + name + ', string)}';
+      }
+    }
+    return '';
+  }
+
+  function handleListFunction_(type, viewArgs, args) {
+    var colName = args[0];
+    var argName, value;
+    if (SQLPoet.isString(args[1])) {
+      argName = args[1];
+      if (args[2] != null) {
+        value = args[2];
+      }
+    } else if (args[1] != null) {
+      var options = args[1];
+      if (options.argName) {
+        argName = options.argName;
+      }
+      if (options.value) {
+        value = options.value;
+      }
+    }
+    // if (!argName) {
+    //   argName = generator();
+    // }
+    if (value != null) {
+      addViewArgs_(viewArgs, type, argName, value);
+    }
+    if (type == 'int') {
+      return '${INT_LIST(' + argName + ', ' + colName + ')}';
+    } else {
+      return '${STRING_LIST(' + argName + ', ' + colName + ')}';
+    }
+  }
+
+  function handleRawListFunction_(viewArgs, args) {
+    var colName = args[0];
+    var argName, value, type;
+    if (SQLPoet.isString(args[1])) {
+      argName = args[1];
+      if (args[2] != null) {
+        value = args[2];
+      }
+      if (args[3] != null) {
+        type = args[3];
+      }
+    } else if (args[1] != null) {
+      var options = args[1];
+      if (options.argName) {
+        argName = options.argName;
+      }
+      if (options.value) {
+        value = options.value;
+      }
+      if (options.type) {
+        type = options.type;
+      }
+    }
+    if (value != null && type == null) {
+      type = typeOf_(value);
+    }
+    if (type == null) {
+      type = 'string';
+    }
+    if (value != null) {
+      addViewArgs_(viewArgs, type, argName, value);
+    }
+    // if (!argName) {
+    //   argName = generator();
+    // }
+    return '${LIST(' + argName + ', ' + type + ', ' + colName + ')}';
+  }
+
+  function addViewArgs_(args, type, name, value) {
+    if (!Array.isArray(value)) {
+      value = [value];
+    }
+    if (type == 'int') {
+      args.put(name, SQLPoet.naInt(value));
+    } else {
+      args.put(name, SQLPoet.naString(value));
+    }
+  }
+
+  function typeOf_(value) {
+    if (Array.isArray(value)) {
+      value = value[0];
+    }
+    if (SQLPoet.isNumber(value)) {
+      return 'int';
+    }
+    return 'string';
+  }
 
   return function(args) {
     var obj = {
